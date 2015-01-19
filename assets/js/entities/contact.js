705e92c9ae1a961d43af6985822121054c373532
Quentin Bahers <quentin.bahers@gmail.com>
2015-01-19 15:17:43 +0000

    Adapt our app to deal with persisted data

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,10 +1,17 @@
 ContactManager.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
-  Entities.Contact = Backbone.Model.extend({});
+  Entities.Contact = Backbone.Model.extend({
+    urlRoot: "contacts"
+  });
+
+  Entities.configureStorage(Entities.Contact);
 
   Entities.ContactCollection = Backbone.Collection.extend({
+    url: "contacts",
     model: Entities.Contact, 
     comparator: "firstName"
   });
+
+  Entities.configureStorage(Entities.ContactCollection);
 
   var contacts;
 
@@ -14,18 +21,35 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
       { id: 2, firstName: "Bob", lastName: "Brigham", phoneNumber: "555-0163" },
       { id: 3, firstName: "Charlie", lastName: "Campbell", phoneNumber: "555-0129" }
     ]);
+    contacts.forEach(function(contact){
+      contact.save();
+    });
+    return contacts;
   };
 
   var API = {
     getContactEntities: function(){
-      if (contacts === undefined){
-	initializeContacts();
+      var contacts = new Entities.ContactCollection();
+      contacts.fetch();
+      if (contacts.length === 0){
+	// if we don't have any contacts yet, create some for convinience
+	return initializeContacts();
       }
       return contacts;
+    }
+    
+    getContactEntity: function(contactId){
+      var contact = new Entities.Contact({id: contactId});
+      contact.fetch();
+      return contact;
     }
   };
 
   ContactManager.reqres.setHandler("contact:entities", function(){
     return API.getContactEntities();
+  });
+
+  ContactManager.reqres.setHandler("contact:entity", function(id){
+    return API.getContactEntity(id);
   });
 });
